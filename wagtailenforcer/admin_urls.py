8724926c350c1baa648a44d481ec823c5786ec04
Loic Teixeira <loic@springload.co.nz>
2018-03-29 11:36:55 +1100

    Fix URL check warning
    (urls.W002) Your URL pattern '^/reset/(\d+)/$' [name='wagtailenforcer_unblock_user'] has a regex beginning with a '/'. Remove this slash as it is unnecessary.

--- wagtailenforcer/admin_urls.py
+++ wagtailenforcer/admin_urls.py
@@ -6,5 +6,5 @@ from wagtailenforcer.views.axes import views as axes_views
 
 urlpatterns = [
     url(r'^$', axes_views.list, name='wagtailenforcer_blocked_users'),
-    url(r'^/reset/(\d+)/$', axes_views.reset, name='wagtailenforcer_unblock_user'),
+    url(r'^reset/(\d+)/$', axes_views.reset, name='wagtailenforcer_unblock_user'),
 ]
